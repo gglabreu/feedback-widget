30705276df58a80eb47b86b60be3f4e8b1db9611
Guilherme Abreu <gglabreu@hotmail.com>
2022-05-05 19:57:05 -0300

    feat: close button and icons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,61 @@
+import { CloseButton } from "./CloseButton";
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "imagem de um balão de pensamento",
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header>
+        <span className="text-xl leading-6 ">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button>
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+
+      <footer>
+        Feito por <span> </span>
+        <a
+          className="underline underline-offset-1"
+          href="https://github.com/gglabreu"
+          target="_blank"
+        >
+          Guilherme Abreu
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
