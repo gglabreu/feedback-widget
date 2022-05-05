d93c096a9edd4306ba358d4abd9248d05f3efb35
Guilherme Abreu <gglabreu@hotmail.com>
2022-05-04 23:33:34 -0300

    feat global bg color

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,11 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm';
 
- 
  export function Widget() {
    return (
      <Popover className='absolute bottom-5 right-5'>
-       <Popover.Panel>Hello World</Popover.Panel>
+       <Popover.Panel> <WidgetForm /> </Popover.Panel>
        <Popover.Button className='bg-brand-500  rounded-full px-3 h-12 text-white flex items-center group' >
          <ChatTeardropDots className='w-6 h-6' />
          <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear' >
@@ -15,4 +15,4 @@ import { Popover } from '@headlessui/react'
       </Popover.Button>
      </Popover>
    )
- }
+}
