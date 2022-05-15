001ad2c1510d6cb1566595fd9fa05dc158894ddc
Guilherme Abreu <gglabreu@hotmail.com>
2022-05-15 20:41:19 -0300

    feat: accessibility tab

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
+
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
 import thoughtImageUrl from "../assets/thought.svg";
@@ -27,7 +30,11 @@ const feedbackTypes = {
   },
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -38,7 +45,12 @@ export function WidgetForm() {
       <div className="flex py-8 gap-2 w-full">
         {Object.entries(feedbackTypes).map(([key, value]) => {
           return (
-            <button>
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => setFeedbackType(key)}
+              type="button"
+            >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
             </button>
