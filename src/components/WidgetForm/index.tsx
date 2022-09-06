6dc5254d38d96774347dfca0bc2eeee75d526dbb
Guilherme Abreu <gglabreu@hotmail.com>
2022-09-05 21:01:40 -0300

    feature: content of feedback types

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,8 +5,9 @@ import { CloseButton } from "../CloseButton";
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -30,7 +31,7 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
@@ -42,23 +43,9 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      {! feedbackType ? (
-              <div className="flex py-8 gap-2 w-full">
-              {Object.entries(feedbackTypes).map(([key, value]) => {
-                return (
-                  <button
-                    key={key}
-                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                    onClick={() => setFeedbackType(key as FeedbackType)}
-                    type="button"
-                  >
-                    <img src={value.image.source} alt={value.image.alt} />
-                    <span>{value.title}</span>
-                  </button>
-                )
-              })  }
-            </div>
-      ) : (
+      {! feedbackType ? ( 
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+       ) : (
         <p>Hello World</p>
       )}
 
