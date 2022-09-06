ec90175ba0f9c1773bd49e81ae9839bc6c5ea719
Guilherme Abreu <gglabreu@hotmail.com>
2022-09-05 21:34:11 -0300

    feature: widget show title of each type of feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,21 @@
-export function FeedbackContentStep(){
-    return ()
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+}
+
+export function FeedbackContentStep({
+  feedbackType,
+}: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  return (
+    <>
+      <header>
+        <span className="text-xl leading-6 ">{feedbackTypeInfo.title}</span>
+        <CloseButton />
+      </header>
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -38,15 +39,11 @@ export function WidgetForm() {
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6 ">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
 
       {! feedbackType ? ( 
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
        ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep feedbackType={feedbackType} />
       )}
 
       <footer>
