b3e79b975626c402829aaff0cb032e32dc7d0888
Guilherme Abreu <gglabreu@hotmail.com>
2022-09-29 00:06:03 -0300

    feat: feedback content back button

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -34,13 +34,19 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedBack() {
+    setFeedbackType(null)
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w[calc(100vw-2rem)] md:w-auto">
 
       {! feedbackType ? ( 
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
        ) : (
-        <FeedbackContentStep feedbackType={feedbackType} />
+        <FeedbackContentStep 
+        feedbackType={feedbackType}
+        onFeedbackRestartRequested={handleRestartFeedBack} />
       )}
 
       <footer>
