e5550f58d04647575af9e9fdaa9d91851a83957f
Guilherme Abreu <gglabreu@hotmail.com>
2022-11-27 19:55:19 -0300

    feat: camera button

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,6 @@
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -39,6 +39,9 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
+          <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
+            <Camera className="w-6 h-6 text-zinc-100" />
+          </button>
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         brand: {
           300: '#996DFF',
           500: '#8257e6'
+        },
+        borderRadius: {
+          md: '4px'
         }
       }
     }
