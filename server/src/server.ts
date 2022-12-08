3034dba072560f781362e5479d442d245a583b87
Guilherme Abreu <gglabreu@hotmail.com>
2022-12-08 19:25:09 -0300

    feat: backend and database

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    return res.send('Hello World')
+})
+
+app.listen(3333, () => {
+    console.log('http server running')
+})
+
+//SQLite
+//Prisma
